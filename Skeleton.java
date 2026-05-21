5ab69252b5fcf6e4dc688fe8fc6800fb1fbaa3fb
Antonio_Jan_Cabuenas <antoniojan.cabuenas@cit.edu>
2026-05-21 16:09:08 +0800

    Delete Skeleton.java

--- Skeleton.java
+++
@@ -1,9 +0,0 @@
-public class Skeleton extends Enemy {
-    public Skeleton() {
-        super("Skeleton", 55, 12, 15, 15);
-    }
-
-    public String getAttackMessage(Entity target) {
-        return "The Skeleton draws a bow and aims at " + target.getName() + "!";
-    }
-}
