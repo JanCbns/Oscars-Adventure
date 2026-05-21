66fc9fce944ab919321afd8e8e5b4ce0c390074f
Antonio_Jan_Cabuenas <antoniojan.cabuenas@cit.edu>
2026-05-21 16:09:31 +0800

    Delete Troll.java

--- Troll.java
+++
@@ -1,9 +0,0 @@
-public class Troll extends Enemy {
-    public Troll() {
-        super("Troll", 65, 13, 15, 18);
-    }
-
-    public String getAttackMessage(Entity target) {
-        return "The Troll roars and hurls rocks at " + target.getName() + "!";
-    }
-}
