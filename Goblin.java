1e61a26419179375bc5c126f0364897891c3fcb2
Antonio_Jan_Cabuenas <antoniojan.cabuenas@cit.edu>
2026-05-21 16:07:20 +0800

    Delete Goblin.java

--- Goblin.java
+++
@@ -1,9 +0,0 @@
-public class Goblin extends Enemy {
-    public Goblin() {
-        super("Goblin", 70, 15, 10, 20);
-    }
-
-    public String getAttackMessage(Entity target) {
-        return "The Goblin shrieks and swings at " + target.getName() + "!";
-    }
-}
