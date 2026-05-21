2880fad899a4925e6a257f3ccb4e1ddf0a189e5d
Antonio_Jan_Cabuenas <antoniojan.cabuenas@cit.edu>
2026-05-21 16:06:56 +0800

    Delete BossEnemy.java

--- BossEnemy.java
+++
@@ -1,9 +0,0 @@
-public class BossEnemy extends Enemy {
-    public BossEnemy() {
-        super("Infernal Witch", 120, 20, 15, 500);
-    }
-
-    public String getAttackMessage(Entity target) {
-        return "⚡ Infernal Witch unleashes a Sunstrike on " + target.getName() + "!";
-    }
-}
